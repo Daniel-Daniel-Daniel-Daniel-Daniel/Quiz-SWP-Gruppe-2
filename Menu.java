3da1bf0cb30d73c668a3e09f6c551c52cfa918ad
Jakob Selb <j.selb0711@gmail.com>
2026-06-03 09:06:10 +0200

    src Dateien

---
+++ Menu.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Menu {
+    public Menu(){
+
+        JFrame frame = new JFrame("Menu");
+
+        Quizwindow qw= new Quizwindow();
+
+        frame.add(quizwindow);
+    }
+}
