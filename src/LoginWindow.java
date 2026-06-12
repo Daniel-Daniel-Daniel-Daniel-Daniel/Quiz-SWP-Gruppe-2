00b15755edd8587e18649194d01dc6b1f0de65c7
SelbJ08 <j.selb0711@gmail.com>
2026-06-12 12:16:05 +0200

    Anmeldefunktion mit Beispiel-User und Beispiel-Passwort

--- src/LoginWindow.java
+++ src/LoginWindow.java
@@ -15,23 +15,49 @@ public class LoginWindow {
 
         JLabel nameLabel = new JLabel("Name:");
         JTextField nameField = new JTextField(15);
-        JLabel passwordLabel = new JLabel("Passwort:");
-        JPasswordField password = new JPasswordField(15);
-        JButton startButton = new JButton("Start");
 
+        JLabel passwordLabel = new JLabel("Passwort:");
+        JPasswordField passwordField = new JPasswordField(15);
+
+        JButton startButton = new JButton("Start");
 
         frame.add(nameLabel);
         frame.add(nameField);
         frame.add(passwordLabel);
-        frame.add(password);
+        frame.add(passwordField);
         frame.add(startButton);
 
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                frame.dispose();
-                new QuizWindow();
+                String username = nameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (username.equals("User 1")
+                        || username.equals("User 2")
+                        || username.equals("User 3")) {
+
+                    if (password.equals("1234")) {
+
+                        frame.dispose();
+                        new QuizWindow();
+
+                    } else {
+
+                        JOptionPane.showMessageDialog(
+                                frame,
+                                "Falsches Passwort!"
+                        );
+                    }
+
+                } else {
+
+                    JOptionPane.showMessageDialog(
+                            frame,
+                            "Benutzername nicht gefunden!"
+                    );
+                }
             }
         });
 
